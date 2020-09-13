f1b14ea886a68172f2d62970bc72bb516230423c
Adam <jsj0828@yahoo.com.tw>
2020-09-13 20:50:58 +0800

    add login viewmodel

---
+++ MemberLogin/Models/ViewModel/LoginModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MemberLogin.Models.ViewModel
+{
+    public class LoginModel
+    {
+        [Required]
+        public string Name { set; get; }
+
+        [Required]
+        public string Password { set; get; }
+    }
+}
