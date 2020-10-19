9a31b0244f67cdbe1ca4d1c6e5907598f88eb014
Adam <jsj0828@yahoo.com.tw>
2020-10-20 02:44:06 +0800

    單元測試修改

--- NUnitTest/UnitTest1.cs
+++ NUnitTest/UnitTest1.cs
@@ -23,7 +23,7 @@ namespace Tests
         public void Test2()
         {
             //測試新增
-            var rtn = new Sys_userService().Insert(new MemberLogin.Models.EF.sys_user { id = 2, Name = "adam", Password = "adam", Level = 1 });
+            var rtn = new Sys_userService().Insert(new MemberLogin.Models.EF.sys_user { id = 999, Name = "testAcc", Password = "adam", Level = 1 });
             Assert.True(rtn == 1);
         }
 
@@ -31,7 +31,7 @@ namespace Tests
         public void Test3()
         {
             //測試更新
-            var rtn = new Sys_userService().Update(new MemberLogin.Models.EF.sys_user { id = 2, Name = "adam", Password = "adambb", Level = 1 });
+            var rtn = new Sys_userService().Update(new MemberLogin.Models.EF.sys_user { id = 999, Name = "testAcc", Password = "adambb", Level = 1 });
             Assert.True(rtn == 1);
         }
 
@@ -39,7 +39,7 @@ namespace Tests
         public void Test4()
         {
             //測試刪除
-            var rtn = new Sys_userService().Delete(2);
+            var rtn = new Sys_userService().Delete(999);
             Assert.True(rtn == 1);
         }
     }
